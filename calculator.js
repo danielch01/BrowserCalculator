a59bddc6f2eb4c0ba2f23f295a1dbaa1a64ba097
Daniel <dhrchoi@gmail.com>
2016-01-31 16:50:52 -0800

    finished calculator

--- calculator.js
+++ calculator.js
@@ -1,21 +1,57 @@
-function add(x, y){
-	return x+y;
-}
-
-function subtract(x, y){
-	return x-y;
-}
-
-function multiply(x, y){
-	return x*y;
-}
-
-function divide(x, y){
-	return x/y;
-}
-
 $(document).ready(function(){
-	$("div").click(function(){
-		
-	})
-})
+	var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        } 
+    };
+
+	var number = "";
+    var newnumber = "";
+    var operator = "";
+    var totaldiv = $("#total");
+    totaldiv.text("0");
+
+    $("#numbers > a").not("#clear,#clearall").click(function(){
+		number += $(this).text();
+		totaldiv.text(number);
+		testNumLength(number);
+    });
+
+    $("#operators > a").not("#equals").click(function(){
+		operator = $(this).text();
+		newnumber = number;
+		number = "";
+		totaldiv.text("0");
+    });
+
+    $("#clear,#clearall").click(function(){
+		number = "";
+		totaldiv.text("0");
+		if ($(this).attr("id") === "clearall") {
+			newnumber = "";
+		}
+    });
+    //Add your last .click() here!
+    $("#equals").click(function(){
+        var x = parseInt(number, 10)
+        var y = parseInt(newnumber, 10)
+        if(operator === "+"){
+            result = y + x;
+        }
+        else if(operator === "-"){
+            result = y - x;
+        }
+        else if (operator === "*"){
+            result = y * x;
+        }
+        else if(operator === "/"){
+            result = y / x;
+        }
+        totaldiv.text(result.toString(10))
+        
+    });
+});
